8eebaa363832f0ccffa4b21d3313061b4990261d
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2023-04-27 17:41:51 -0400

    first cut of actualy data being populated

--- src/js/plugin.js
+++ src/js/plugin.js
@@ -1,24 +1,21 @@
-/******************************************************************************
- *
- * Copyright (c) 2017, the Perspective Authors.
- *
- * This file is part of the Perspective library, distributed under the terms of
- * the Apache License 2.0.  The full license can be found in the LICENSE file.
- *
- */
-
 import STYLE from "../../dist/css/perspective-viewer-summary.css";
+
+const _ALIGN_DEFAULT = "horizontal";
 
 export class PerspectiveViewerSummaryPluginElement extends HTMLElement {
   constructor() {
     super();
+    this._config = {
+      align: _ALIGN_DEFAULT,
+    };
   }
 
   connectedCallback() {
     if (!this._container) {
       this._container = document.createElement("div");
+      this._container.classList.add("summary-container");
     }
-    this.parentElement.appendChild(this._container);
+    this.appendChild(this._container);
   }
 
   disconnectedCallback() {}
@@ -26,37 +23,125 @@ export class PerspectiveViewerSummaryPluginElement extends HTMLElement {
   async activate(view) {}
 
   get name() {
+    // Plugin name is "Summary"
     return "Summary";
   }
 
   get select_mode() {
-    return "toggle";
+    // select any number of cols
+    return "select";
   }
 
   get min_config_columns() {
+    // at least 1 column
     return 1;
   }
 
   async draw(view) {
-    const count = await view.num_rows();
-    this.innerHTML = `View has ${count} rows`;
+    await this.render(view);
+  }
+  async update(view) {
+    await this.render(view);
   }
 
-  async update(view) {}
+  async render(view) {
+    // pull columns
+    const columns = (await view.get_config()).columns;
+    const data_window = {
+      start_row: 0,
+      start_col: 0,
+      end_row: 1,
+      end_col: columns.length,
+      id: false,
+    };
+    const data = await view.to_columns(data_window);
+    const is_pivoted = data.__ROW_PATH__ && data.__ROW_PATH__.length > 0;
 
-  async resize() {}
+    const _entries = new Map();
 
-  async clear() {}
+    columns.forEach((col) => {
+      // container for data
+      const col_container = document.createElement("div");
+      col_container.classList.add("summary-column");
 
-  save() {}
+      // header
+      const header_container = document.createElement("div");
+      header_container.classList.add("summary-header");
 
-  restore(token) {}
+      const header_data = document.createElement("span");
+      header_data.classList.add("summary-header-text");
+      header_data.textContent = col;
+      header_container.appendChild(header_data);
 
-  async restyle(view) {}
+      // data
+      const data_container = document.createElement("div");
+      data_container.classList.add("summary-data");
 
-  delete() {}
+      const data_data = document.createElement("span");
+      data_data.classList.add("summary-data-text");
 
-  // Private
+      data_container.appendChild(data_data);
+      // add to container
+      col_container.appendChild(header_container);
+      col_container.appendChild(data_container);
+
+      // put in map
+      _entries.set(col, col_container);
+
+      if (is_pivoted) {
+        data_data.textContent = data[col];
+      } else {
+        data_data.textContent = "--";
+      }
+
+      // set class based on alignment
+      this.align();
+    });
+
+    // clear it out
+    while (this._container.lastChild) {
+      this._container.removeChild(this._container.lastChild);
+    }
+
+    // and refill
+    _entries.forEach((value, key) => {
+      this._container.appendChild(value);
+    });
+  }
+
+  async resize() {
+    // TODO nothing yet
+  }
+
+  async clear() {
+    // TODO
+  }
+
+  save() {
+    // TODO
+    return { ...this._config };
+  }
+
+  restore(token) {
+    // TODO
+    const align = token.align || _ALIGN_DEFAULT;
+    this._config.align = align;
+    this.align();
+  }
+
+  align() {
+    this._container.classList.remove("align-horizontal");
+    this._container.classList.remove("align-vertical");
+    this._container.classList.add(`align-${this._config.align}`);
+  }
+
+  async restyle(view) {
+    // TODO
+  }
+
+  delete() {
+    // TODO
+  }
 }
 
 customElements.define(
@@ -64,22 +149,11 @@ customElements.define(
   PerspectiveViewerSummaryPluginElement
 );
 
-/**
- * Appends the default table CSS to `<head>`, should be run once on module
- * import.
- *
- */
 function _register_global_styles() {
   const style = document.createElement("style");
   style.textContent = STYLE;
   document.head.insertBefore(style, document.head.firstChild);
 }
-
-/******************************************************************************
- *
- * Main
- *
- */
 
 function register_element() {
   customElements
